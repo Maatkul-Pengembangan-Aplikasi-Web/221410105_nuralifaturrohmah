b87c6e6f2575f4e5f5269493c76d0557b957f3ac
Nuralifaturrohmah <alifaturrohmah3103@gmail.com>
2024-10-28 14:26:23 +0700

    fungsi tambah data add

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -9,7 +9,27 @@ use Illuminate\Http\Request;
 class ProdiController extends Controller
 {
     public function index()
+    {$prodis = Prodi::orderBy('id', 'desc')->get();
+        return view('prodi.index', compact('prodis'));
+    }
+    
+    public function create()
     {
-    return view('prodi.index');
+        return view('prodi.create');
+    }
+    
+    public function save(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required'
+        ]);
+    
+        Prodi::create([
+            'nama' => $request->nama
+        ]);
+    
+        return redirect()->route('/prodi')->with('success', 'Program Studi berhasil ditambahkan');
     }
 }
+
+  
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/prodi', [ProdiController::class, 'index'])->name('/prodi');
+    Route::get('/prodi/create', [ProdiController::class, 'create'])->name('prodi/create');
+Route::post('/prodi/save', [ProdiController::class, 'save'])->name('prodi/save');
 });
 
 require __DIR__.'/auth.php';
